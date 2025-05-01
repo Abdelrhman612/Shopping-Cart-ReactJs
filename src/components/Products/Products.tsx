01316209b5cec742198d9503efd8f77a8747bb51
Abdelrhman <abdo612200320@gmail.com>
2025-05-01 14:36:30 +0300

    update Products.tsx

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,46 +1,21 @@
 import { InterFaceProducts } from "./interFace";
-import starIcon from "../../assets/icons/icons8-star-48.png";
-
+import Product from "./product";
 interface ProductsProps {
   products: InterFaceProducts[];
+  setCartItems: React.Dispatch<React.SetStateAction<InterFaceProducts[]>>;
 }
 
-const Products = ({ products }: ProductsProps) => {
+const Products = ({ products, setCartItems }: ProductsProps) => {
   return (
     <div>
       <ul style={{ display: "flex", gap: "15px", listStyle: "none" }}>
         {products.map((product) => {
           return (
-            <li key={product.id}>
-              <img
-                src={product.img}
-                alt={product.title}
-                width="100px"
-                height="100px"
-              />
-              <p>{product.title}</p>
-              <p>
-                rate: {product.rate}
-                {Array(product.rate)
-                  .fill(0)
-                  .map((index) => (
-                    <img
-                      key={index}
-                      src={starIcon}
-                      alt="star icon"
-                      width="15px"
-                      height="15px"
-                    />
-                  ))}
-              </p>
-              <p>Price: {Number(product.price).toLocaleString()} جنيه</p>
-              <p>
-                insteadOf:{" "}
-                <span style={{ textDecoration: "line-through" }}>
-                  {product.insteadOf} جنيه
-                </span>
-              </p>
-            </li>
+            <Product
+              key={product.id}
+              product={product}
+              setCartItems={setCartItems}
+            />
           );
         })}
       </ul>
